52273fcf933d4142959c3af1b0e8bf8f28033a79
turbyfilllw <112126736+turbyfilllw@users.noreply.github.com>
2022-11-15 21:31:05 -0500

    Add files via upload

---
+++ Lab 6/Lab 6/Book.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Lab_6
+{
+	public class Book
+	{
+		public string Id { get; set; }
+		public VolumeInfo VolumeInfo { get; set; }
+		public string SelfLink { get; set; }
+
+		public Book()
+		{
+		}
+
+		public Book(string id, VolumeInfo volumeInfo, string selflink)
+		{
+			this.Id = id;
+			this.VolumeInfo = volumeInfo;
+			this.SelfLink = selflink;
+		}
+
+        public override string ToString()
+        {
+			return $"Id: {Id}\n" +
+				   $"Title: {VolumeInfo.Title}\n"+
+				   $"Author: {string.Join(",",VolumeInfo.Authors)}\n" +
+				   $"Description: {VolumeInfo.Description}\n" +
+				   $"Selflink: {SelfLink}\n";
+        }
+    }
+}
+
---
+++ Lab 6/Lab 6/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace Lab_6;
+class Program
+{
+    private static List<Book> books = new List<Book>();
+    static void Main(string[] args)
+    {
+        string jsonString = string.Empty;
+        var root = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString();
+        var dataFolder = root + Path.DirectorySeparatorChar + "Data";
+        var fileNames = new List<string>();
+
+        var serializeFolder = root + Path.DirectorySeparatorChar + "Serialized";
+        var writeJSON = serializeFolder + Path.DirectorySeparatorChar + "books.JSON";
+
+        foreach (var file in Directory.GetFiles(dataFolder))
+        {
+            fileNames.Add(file);
+        }
+
+        foreach (var book in ReadBookFile(fileNames))
+        {
+            Console.WriteLine(book);
+        }
+
+
+        WriteJsonToFile(writeJSON);
+        Console.ReadKey();
+    }
+
+    private static string WriteJsonToFile(string writeJSON)
+    {
+        string jsonString;
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        jsonString = JsonSerializer.Serialize(books, options);
+
+        using (var sw = new StreamWriter(writeJSON, false))
+        {
+            sw.WriteLine(jsonString);
+        }
+
+        return jsonString;
+    }
+
+    static List<Book> ReadBookFile(List<string> fileNames)
+    {
+        //var books = new List<Book>();
+        
+        foreach(var file in fileNames)
+        {
+            string jsonString = string.Empty;
+            using (var sr = new StreamReader(file))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var book = JsonSerializer.Deserialize<Book>(jsonString, options);
+            books.Add(book);
+        }
+
+        return books;
+    }
+}
+
